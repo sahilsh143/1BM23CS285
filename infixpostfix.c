294f86b5602832bb3be3570b2d322752bed76279
sahilsh143 <sahilsharma.cs23@bmsce.ac.in>
2024-10-16 10:24:38 +0530

    Add files via upload

---
+++ infixpostfix.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+
+void infixToPostfix(char infix[], char postfix[]) {
+    int i = 0, k = 0;
+
+    while (infix[i] != '\0') {
+        char ch = infix[i];
+
+
+        if (ch >= 'a' && ch <= 'z') {
+            postfix[k++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (peek() != '(') {
+                postfix[k++] = pop();
+            }
+            pop();
+        }
+        else {
+            while (top != -1 && precedence(peek()) >= precedence(ch)) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+        i++;
+    }
+
+    while (top != -1) {
+        postfix[k++] = pop();
+    }
+
+    postfix[k] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter a valid parenthesized infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
